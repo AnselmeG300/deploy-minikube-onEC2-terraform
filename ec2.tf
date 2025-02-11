966110eec354c455d6da124f948e4790dfa667e6
Anselme Gildas Tchassem Boutchouang <43468327+AnselmeG300@users.noreply.github.com>
2025-02-11 19:23:12 +0000

    update remte exec

--- ec2.tf
+++ ec2.tf
@@ -36,32 +36,30 @@ resource "aws_instance" "myec2" {
     inline = [
       "sudo sed -i -e 's/mirror.centos.org/vault.centos.org/g' -e 's/^#.*baseurl=http/baseurl=http/g' -e 's/^mirrorlist=http/#mirrorlist=http/g' /etc/yum.repos.d/*.repo",
 
-      "sudo yum update -y", 
-      "sudo yum -y install epel-release",
-      "sudo yum install curl unzip git wget -y",
       
-      "sudo curl -fsSL https://get.docker.com -o get-docker.sh",
-      "sudo sh get-docker.sh",
-      "sudo usermod -aG docker centos",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-
-      "sudo yum -y install nano git libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils",
+      "sudo yum -y update",
+      "sudo yum -y install epel-release",
+      "sudo yum -y install git libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils",
       "sudo yum install socat -y",
       "sudo yum install -y conntrack",
-
-      "sudo wget https://storage.googleapis.com/minikube/releases/v1.11.0/minikube-linux-amd64",
+      "curl -fsSL https://get.docker.com -o install-docker.sh",
+      "sh install-docker.sh --dry-run",
+      "sudo sh install-docker.sh",
+      "sudo usermod -aG docker centos",
+      "sudo systemctl start docker",
+      "sudo yum -y install wget",
+      "sudo wget https://storage.googleapis.com/minikube/releases/v1.32.0/minikube-linux-amd64",
       "sudo chmod +x minikube-linux-amd64",
       "sudo mv minikube-linux-amd64 /usr/bin/minikube",
-      "sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.0/bin/linux/amd64/kubectl",
+      "sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.29.0/bin/linux/amd64/kubectl",
       "sudo chmod +x kubectl",
-      "sudo mv kubectl /usr/bin/",
+      "sudo mv kubectl  /usr/bin/",
       "sudo su",
-      "sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables",
+      "echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables",
       "exit",
       "sudo systemctl enable docker.service",
       "exec sudo su -l $USER",
-      "minikube start –driver=docker --kubernetes-version=v1.28.3"
+      "minikube start –driver=docker --kubernetes-version=v1.28.3",
 
     ]
   }
